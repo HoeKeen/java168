2011b8788b39a9239a418e6e6c585972da8dd2aa
HowKeen <soriheda06@gmail.com>
2020-06-10 17:14:22 +0900

    오후 3:51 2020-06-10

--- race/PlayerApp.java
+++ race/PlayerApp.java
@@ -14,6 +14,23 @@ public class PlayerApp {
 		}
 	for(int i=0;i<team.length;i++) {
 		team[i].state();
-	}
+		}
+	
+	Player[] ranking = new Player[3];
+	
+	if(team[0].getRecord()<team[1].getRecord()) {
+
+		if(team[0].getRecord()<team[2].getRecord()) { 
+			ranking[0] = team[0];		
+		}else{
+			ranking[0] = team[2];
+		}
+	}else{
+		if(team[1].getRecord()<team[2].getRecord()) 
+			  ranking[0] = team[1];
+		
+		else {ranking[0] = team[2];} 
+		}
+	System.out.println("1등은 "+ranking[0].getName()+"입니다.");
 	}
 }
